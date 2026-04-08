8ddc98702bbd2f0f49a7c97c2aee331c0b83c79e
Tiago Covas <tiagocovas2004@gmail.com>
2026-04-08 01:59:55 +0100

    Unblur

---
+++ ProjectHub.Domain/Services/UnblurObjective.cs
@@ -0,0 +1,26 @@
+﻿using ProjectHub.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectHub.Domain.Services
+{
+    public Project UnblurObjective(Project project, int index)
+    {
+        // 1. Vai buscar o objetivo atual.
+        var currentObjective = project.Objectives[index];
+
+        // 2. Cria uma CÓPIA do objetivo sem o blur (usando 'with').
+        var updatedObjective = currentObjective with { IsBlurred = false };
+
+        // 3. Como é que criarias uma NOVA versão do 'project' 
+        // com a lista de objetivos atualizada, sem usar um loop 'for'?
+
+        return project with
+        {
+            Objectives = project.Objectives.SetItem(index, updatedObjective)
+        };
+    }
+}
