1d2146c1ab48e0e98fc601ed64d1b1324df55ab2
Tiago Covas <tiagocovas2004@gmail.com>
2026-04-08 01:42:54 +0100

    Adicionar arquivos de projeto.

---
+++ ProjectHub.Domain/Models/Project.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectHub.Domain.Models
+{
+    public record Project(
+        Guid Id,
+        String Name,
+        IReadOnlyList<Objective> Objectives);
+
+    public record Objective(
+        string description,
+        bool isCompleted);
+
+}
